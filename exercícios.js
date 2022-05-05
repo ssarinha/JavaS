de55d8ade827c946e080a7710c1d806e5beeecb7
ssarinha <102757853+ssarinha@users.noreply.github.com>
2022-05-05 15:45:43 -0300

    Add files via upload

---
+++ exercícios.js
@@ -0,0 +1,33 @@
+// 1-a
+console.log (10 + 20 * 30)
+
+// 1-b
+console.log (4**2/30)
+
+//1-c
+console.log ((9**4+2)*6-1)
+
+//2
+console.log (10%3*10**1+1-10*4/2)
+
+//3
+console.log('Sara' + ' ' + 'de' + ' ' + 'Sousa' + ' ' + 'Lima');        
+
+//4
+const x=3
+const y=5
+
+console.log (2*x*3*y)
+
+//5
+const a=1;
+const b=2;
+const c=3;
+
+console.log (a+b+c)
+
+//6
+const salário=1800;
+const aumento=2;
+
+console.log (salário*aumento)
